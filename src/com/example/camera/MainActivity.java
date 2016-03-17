c0fd224ab23f28e2619f51578901acee9fb37289
jinqiao94 <616698865@qq.com>
2016-03-17 18:43:35 +0800

    camera
    AndroidCamera

---
+++ src/com/example/camera/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.camera;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+/*
+ * 　java.lang.Object/android.view.View/android.widget.ImageView　　
+已知直接子类：ImageButton, QuickContactBadge 
+显示任意图像，例如图标。ImageView类可以加载各种来源的图片（如资源或图片库），
+需要计算图像的尺寸，比便它可以在其他布局中使用，并提供例如缩放和着色（渲染）各种显示选项。
+ * */
+	ImageView imgFavorite;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgFavorite=(ImageView)findViewById(R.id.imageView1);
+        imgFavorite.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		open();
+        	}
+        });
+    }
+        public void open() {
+		// TODO Auto-generated method stub
+        	Intent intent=new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent,0);
+	}
+        @Override
+	 protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        	super.onActivityResult(requestCode, resultCode, data);
+		//命名要注意，get方法里面的引号不要忘了,activity不要再写错了！
+		 
+		 Bitmap bp=(Bitmap)data.getExtras().get("data");
+		 imgFavorite.setImageBitmap(bp);
+	 }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
